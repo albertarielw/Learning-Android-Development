771b11eb1a8a2b27719f87efc69d6c4cd7550081
albertarielw <albertariel2002@gmail.com>
2022-08-29 13:28:51 +0800

    add Kotlin practice

---
+++ Kotlin/themetutorial/app/src/main/java/com/auth0/theme_tutorial/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.auth0.theme_tutorial
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
